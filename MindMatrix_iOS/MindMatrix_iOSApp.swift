cbb674b03a41a95b2981e4da12f67194ae5ed99a
David Gilvan Souza <97628609+DGjiban@users.noreply.github.com>
2024-10-09 13:23:59 -0400

    Initial Commit

---
+++ MindMatrix_iOS/MindMatrix_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindMatrix_iOSApp.swift
+//  MindMatrix_iOS
+//
+//  Created by David Gilvan Dos Santos Souza on 2024-10-09.
+//
+
+import SwiftUI
+
+@main
+struct MindMatrix_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
